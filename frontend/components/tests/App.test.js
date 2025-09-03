e17844055aacc74f12577e5101736264bdf2db91
Dineury <DineuryLopez1@gmail.com>
2025-09-03 18:46:17 -0400

    Finished module project

--- frontend/components/tests/App.test.js
+++ frontend/components/tests/App.test.js
@@ -1,6 +1,6 @@
 // 👇 YOUR WORK STARTS ON LINE 19
 import React from 'react'
-import { render, waitFor, screen } from '@testing-library/react'
+import { render, waitFor, screen, fireEvent, getByText, queryByText, findByText } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import server from '../../../backend/mock-server'
@@ -16,28 +16,58 @@ describe('Stranger Things App', () => {
     user = userEvent.setup()
   })
   test('App mounts without crashing', () => {
+    const image = screen.getByRole('img')
+    const button = screen.getByRole('button')
+    expect(image).toBeInTheDocument()
+    expect(button).toHaveTextContent(/Press to Get Show Data/i)
     // 👉 TASK: print the simulated DOM using screen.debug
   })
   test('App renders the correct texts', async () => {
     // 👉 TASK: click on the button that displays "Press to Get Show Data"
-
+    const button = screen.getByText('Press to Get Show Data')
+    fireEvent.click(button)
+   
     // 👉 TASK: create a waitFor and await for the following to be true:
-    //    - The text "Press to Get Show Data" is no longer in the DOM
-    //    - The text "Stranger Things" exists in the DOM
-    //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
-    //    - The text "Select A Season" exists in the DOM
-    // ❗ You will need { exact: false } to select the longer text
+    await waitFor(() => {
+       //    - The text "Press to Get Show Data" is no longer in the DOM
+      expect(button).not.toBeInTheDocument()
+      const strangerHEading = screen.queryByText("Stranger Things")
+      //    - The text "Stranger Things" exists in the DOM
+      expect(strangerHEading).toBeInTheDocument()
 
-    // 👉 TASK: select Season 2 from the dropdown
-    // ❗ Don't forget user actions need the await keyword
-    // ❗ Use the selectOptions user action
-    // ❗ Grab the select element using querySelector
+      //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
+      // ❗ You will need { exact: false } to select the longer text
+      const aloveLetterText = screen.getByText("80s classics that captivated a generation", { exact:false })
+      expect(aloveLetterText).toBeInTheDocument()
+      //    - The text "Select A Season" exists in the DOM
+      const selectSeasonText = screen.getByText("Select A Season")
+      expect(selectSeasonText).toBeInTheDocument()
+     })
 
-    // 👉 TASK: create the following assertions:
-    //    - The text "Season 2, Episode 1" exists in the DOM
-    //    - The text "Chapter One: MADMAX" exists in the DOM
-    //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
-    // ❗ You will need { exact: false } to select the longer text
+     await waitFor(()=> {
+       // 👉 TASK: select Season 2 from the dropdown
+       // ❗ Don't forget user actions need the await keyword
+       // ❗ Use the selectOptions user action
+       // ❗ Grab the select element using querySelector
+       const dropdown = document.querySelector('#seasons')
+       
+      userEvent.click(dropdown)
+      userEvent.selectOptions(dropdown, '1')
+      screen.debug()
+
+      // 👉 TASK: create the following assertions:
+      //    - The text "Season 2, Episode 1" exists in the DOM
+      const season2Ep1Txt = screen.getByText('Season 2, Episode 1')
+      expect(season2Ep1Txt).toBeInTheDocument()
+      //    - The text "Chapter One: MADMAX" exists in the DOM
+      const madMaxText = screen.getByText('Chapter One: MADMAX')
+      expect(madMaxText).toBeInTheDocument()
+      //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
+      // ❗ You will need { exact: false } to select the longer text
+      const oneYearAfterText = screen.getByText("One year after the events with the Upside Down and the Demogorgon", { exact: false })
+      expect(oneYearAfterText).toBeInTheDocument()
+    })
+    
 
   })
 })
--- frontend/components/tests/Episode.test.js
+++ frontend/components/tests/Episode.test.js
@@ -26,28 +26,32 @@ their own search, and meet a mysterious girl in the forest.",
 describe('Episode component', () => {
   test("renders without error", () => {
     // 👉 TASK: render the component passing episode data
-
+      render(<Episode episode={exampleEpisodeData} />)
     // 👉 TASK: print the simulated DOM using screen.debug
-
   })
   test("renders texts and alt texts correctly", () => {
     // 👉 TASK: render the component passing episode data and getting the rerender utility
-
+    const { rerender } = render(<Episode  episode={exampleEpisodeData} />)
     // 👉 TASK: check that the summary renders to the DOM
-
+    expect(screen.getByText(/A young boy mysteriously disappears/i)).toBeInTheDocument()
     // 👉 TASK: check that the alt text "episode image" is present
-
+    const altText = screen.getByAltText("episode image")
+    expect(altText).toBeInTheDocument()
     // 👉 TASK: rerender the component passing episode data lacking an image
     // ❗ Study the Episode component to understand what happens in this case
-
+    const { image, ...rest } = exampleEpisodeData
+    rerender(<Episode episode={rest} />)
+    
     // 👉 TASK: check that the default image appears in the DOM
     // ❗ Use querySelector to select the image by its src attribute
-
+    expect(document.querySelector('img[src="https://i.ibb.co/2FsfXqM/stranger-things.png"')).toBeInTheDocument()
     // 👉 TASK: check that the "generic episode image" alt text is present
-
+    screen.getByAltText('generic episode image')
     // 👉 TASK: rerender the component passing an undefined episode
     // ❗ Study the Episode component to understand what happens in this case
-
+    rerender(<Episode/>)
+    screen.debug()
+      screen.getByText("Loading episode...")
     // 👉 TASK: check that the "Loading episode..." text is present
 
   })
